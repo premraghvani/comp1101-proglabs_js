47f4366518f3aa95315ac592a8ef0825cf2a25cc
Prem Raghvani <69864865+premraghvani@users.noreply.github.com>
2024-11-15 11:48:28 +0000

    custom word banks added

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -1,13 +1,37 @@
 let button = document.getElementById("squeeze");
 
 // words generated from getrandomthings.com, put into ChatGPT to convert into array
-let verbs = ["compete", "replace", "punish", "book", "queue", "wash", "bare", "ignore", "plug", "tempt", "perform", "concern", "reject", "nest", "whisper", "box", "sneeze", "contain", "test", "stamp", "fancy", "harass", "prevent", "shade", "plug", "steer", "deserve", "decorate", "hope", "challenge", "preserve", "concentrate", "glow", "ruin", "promise", "tap", "tire", "queue", "battle"];
-let nouns = ["shoes", "wind", "quiver", "cave", "impulse", "woman", "plastic", "act", "burst", "insect", "cow", "bee", "judge", "jewel", "change", "ship", "battle", "blood", "nerve", "cover", "stocking", "purpose", "coat", "basketball", "pigs", "form", "guide", "chance", "mother", "cloth"];
-let adjectives = ["lopsided", "filthy", "supreme", "icy", "parched", "breezy", "discreet", "conscious", "excited", "boundless", "yellow", "hissing", "jumpy", "fearless", "calculating", "romantic", "magical", "ashamed", "old", "cheap", "debonair", "agreeable", "empty", "frightening", "cynical", "cooperative", "hard", "political", "thundering", "modern"];
-let adverbs = ["generally", "questioningly", "upliftingly", "forth", "smoothly", "anyway", "seemingly", "terribly", "devotedly", "generally", "broadly", "crossly", "knowledgeably", "faithfully", "strongly", "perfectly", "tomorrow", "rarely", "enormously", "essentially", "shrilly", "vacantly", "longingly", "irritably", "sleepily", "worriedly", "commonly", "rudely", "blindly", "vainly"];
+let verbs = []; let adverbs = []; let adjectives = []; let nouns = [];
+
+// resets words
+function wordReset(){
+    verbs = ["compete", "replace", "punish", "book", "queue", "wash", "bare", "ignore", "plug", "tempt", "perform", "concern", "reject", "nest", "whisper", "box", "sneeze", "contain", "test", "stamp", "fancy", "harass", "prevent", "shade", "plug", "steer", "deserve", "decorate", "hope", "challenge", "preserve", "concentrate", "glow", "ruin", "promise", "tap", "tire", "queue", "battle"];
+    nouns = ["shoes", "wind", "quiver", "cave", "impulse", "woman", "plastic", "act", "burst", "insect", "cow", "bee", "judge", "jewel", "change", "ship", "battle", "blood", "nerve", "cover", "stocking", "purpose", "coat", "basketball", "pigs", "form", "guide", "chance", "mother", "cloth"];
+    adjectives = ["lopsided", "filthy", "supreme", "icy", "parched", "breezy", "discreet", "conscious", "excited", "boundless", "yellow", "hissing", "jumpy", "fearless", "calculating", "romantic", "magical", "ashamed", "old", "cheap", "debonair", "agreeable", "empty", "frightening", "cynical", "cooperative", "hard", "political", "thundering", "modern"];
+    adverbs = ["generally", "questioningly", "upliftingly", "forth", "smoothly", "anyway", "seemingly", "terribly", "devotedly", "generally", "broadly", "crossly", "knowledgeably", "faithfully", "strongly", "perfectly", "tomorrow", "rarely", "enormously", "essentially", "shrilly", "vacantly", "longingly", "irritably", "sleepily", "worriedly", "commonly", "rudely", "blindly", "vainly"];
+
+    document.getElementById("nouns").value = nouns.join("\n")
+    document.getElementById("verbs").value = verbs.join("\n")
+    document.getElementById("adjectives").value = adjectives.join("\n")
+    document.getElementById("adverbs").value = adverbs.join("\n")
+}
+
+// adds new words
+function updateWords(){
+    verbs = document.getElementById("verbs").value.split("\n")
+    nouns = document.getElementById("nouns").value.split("\n")
+    adjectives = document.getElementById("adjectives").value.split("\n")
+    adverbs = document.getElementById("adverbs").value.split("\n")
+}
+
 
 // generater function
 function generateSentence(){
+    if(verbs.length == 0 || nouns.length == 0 || adjectives.length == 0 || adverbs.length == 0){
+        alert("There must be at least one verb, noun, adjective and adverb specified. Could not create sentence.")
+        return;
+    }
+
     let chosen = {
         verb: verbs[generateRandomNum(verbs.length)],
         noun: nouns[generateRandomNum(nouns.length)],
@@ -37,3 +61,6 @@ button.addEventListener("click", shout);
 
 // adds listener to sentence generator button
 document.getElementById("sentenceGenButton").addEventListener("click",generateSentence)
+
+// on load, resets words
+wordReset();
